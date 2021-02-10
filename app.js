cfc3fd564869119b26c0954c3adb6420d3e19dbc
Karthik-Nayak98 <karunayak63@gmail.com>
2021-02-10 22:50:46 +0530

    feat: Add responsive navbar

---
+++ app.js
@@ -0,0 +1,7 @@
+const nav = document.querySelector('.navbar');
+
+const btn = document.querySelector('.hamburger');
+
+btn.addEventListener('click', function () {
+  nav.style.display = 'flex';
+});
